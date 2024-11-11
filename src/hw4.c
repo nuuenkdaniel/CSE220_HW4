cb9af5c5dae71800c0ed78a440ac5fba2d8a16bd
Daniel <lewdaniel2@gmail.com>
2024-11-10 22:44:07 -0500

    Modularized and created 2nd socket

--- src/hw4.c
+++ src/hw4.c
@@ -8,59 +8,92 @@
 #include <sys/socket.h>
 
 #define BUFFER_SIZE 1024
-#define PORT 8080
+#define PORT1 2201
+#define PORT2 2202
 
-int main() {
-  int listen_fd, conn_fd;
-  struct sockaddr_in address;
-  int opt = 1;
-  int addrlen = sizeof(address);
-  char buffer[BUFFER_SIZE] = {0};
-
+// Sets up the socket for connection
+void socket_setup(int *listen_fd, struct sockaddr_in *address, int *opt, int *addrlen, short port) {
   // Creating socket
-  if((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+  if((*listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
     perror("Could not create socket");
     exit(EXIT_FAILURE);
   }
 
   // Setting socket options
-  if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+  if(setsockopt(*listen_fd, SOL_SOCKET, SO_REUSEADDR, opt, sizeof(*opt))) {
     perror("Failed to set reuse address option for socket");
     exit(EXIT_FAILURE);
   }
-  if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt))) {
+  if(setsockopt(*listen_fd, SOL_SOCKET, SO_REUSEPORT, opt, sizeof(*opt))) {
     perror("Failed to set reuse port option for socket");
     exit(EXIT_FAILURE);
   }
 
   // Binding socket to port
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(PORT);
-  if(bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+  address->sin_family = AF_INET;
+  address->sin_addr.s_addr = INADDR_ANY;
+  address->sin_port = htons((port == 0)? PORT1 : PORT2);
+  if(bind(*listen_fd, (struct sockaddr *)address, sizeof(*address)) < 0) {
     perror("Failed to bind socket to port");
     exit(EXIT_FAILURE);
   }
 
   // Listening for connections
-  if(listen(listen_fd, 2) < 0) {
-     perror("Failed to listen for connections");
-     exit(EXIT_FAILURE);
-  }
-  printf("[Server] listening on port: %d\n", PORT);
-  
-  // Accept incoming connetions
-  if((conn_fd = accept(listen_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-    perror("Failed to accept connection"); 
+  if(listen(*listen_fd, 2) < 0) {
+    perror("Failed to listen for connections");
     exit(EXIT_FAILURE);
   }
+}
+
+// Accept incoming connections on specified socket
+void await_conn(int *listen_fd, int *conn_fd, struct sockaddr_in *address, int *addrlen) {
+  if((*conn_fd = accept(*listen_fd, (struct sockaddr *)address, (socklen_t *)addrlen)) < 0) {
+    perror("Failed to accept connection");
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main() {
+  int listen_fd1, listen_fd2;
+  int conn_fd1, conn_fd2;
+  struct sockaddr_in address1, address2;
+  int opt = 1;
+  int addrlen = sizeof(address1);
+  char buffer[BUFFER_SIZE] = {0};
+  
+  // Socket setup
+  socket_setup(&listen_fd1, &address1, &opt, &addrlen, 0);
+  socket_setup(&listen_fd2, &address2, &opt, &addrlen, 1);
+  printf("[Server] listening on port %d and %d\n", PORT1, PORT2);
+
+  // Accept Connections
+  await_conn(&listen_fd1, &conn_fd1, &address1, &addrlen);
+  await_conn(&listen_fd2, &conn_fd2, &address2, &addrlen);
+
+  int nbytes;
 
   while(1) {
-    // do something
+    memset(buffer, 0, BUFFER_SIZE);
+    nbytes = read(conn_fd1, buffer, BUFFER_SIZE);
+    if(nbytes <= 0) {
+      perror("Failed to read from client");
+      exit(EXIT_FAILURE);
+    }
+    printf("Data recieved from client1: %s\n", buffer);
+
+    memset(buffer, 0, BUFFER_SIZE);
+    nbytes = read(conn_fd1, buffer, BUFFER_SIZE);
+    if(nbytes <= 0) {
+      perror("Failed to read from client");
+      exit(EXIT_FAILURE);
+    }
+    printf("Data recieved from client2: %s\n", buffer);
   }
 
   printf("[Server] Shutting down.\n");
-  close(conn_fd);
-  close(listen_fd);
+  close(conn_fd1);
+  close(listen_fd1);
+  close(conn_fd2);
+  close(listen_fd2);
   return EXIT_SUCCESS;
 }
