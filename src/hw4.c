a068b9f654dfea7ce24ed321007254af3a900f26
Daniel Lew <lewdaniel2@gmail.com>
2024-11-12 10:14:58 -0500

    debugging rotate shape

--- src/hw4.c
+++ src/hw4.c
@@ -55,12 +55,21 @@ void await_conn(int *listen_fd, int *conn_fd, struct sockaddr_in *address, int *
 
 // Rotates the given shape by the give number of times clockwise
 int *rotate_shape(int *shape, int r, int c, int rotations){ 
-  int temp_shape[16] = {0};
+  int temp_shape[6] = {0};
   memcpy(temp_shape, shape, r*c*sizeof(int));
   int *new_shape = calloc(r*c, sizeof(r*c));
   int temp;
   int curr_left;
   int curr_right;
+
+  for(int i = 0; i < 2; i++) {
+    for(int j = 0; j < 3; j++) {
+      printf("%d ", temp_shape[i*4+j]);
+    }
+    printf("\n");
+  }
+
+
   for(int i = 0; i < rotations; i++) {
     // Transposing the matrix
     for(int j = 0; j < r; j++) {
@@ -70,6 +79,10 @@ int *rotate_shape(int *shape, int r, int c, int rotations){
         temp_shape[k*c+j] = temp;
       }
     }
+    temp = r;
+    r = c;
+    c = temp;
+    // Flipping the columns of the matrix
     for(int j = 0; j < r; j++) {
       curr_left = 0;
       curr_right = c-1;
@@ -77,6 +90,8 @@ int *rotate_shape(int *shape, int r, int c, int rotations){
         temp = temp_shape[j*c+curr_left];
         temp_shape[j*c+curr_left] = temp_shape[j*c+curr_right];
         temp_shape[j*c+curr_right] = temp;
+        curr_left++;
+        curr_right--;
       }
     }
   }
@@ -112,6 +127,18 @@ int main() {
   int shape6[3][2] = { 0,1,0,1,1,1 };
   int shape7[2][3] = { 1,1,1,0,1,0 };
 
+  printf("Got here\n");
+  int *test_shape = rotate_shape((int*)shape3, 2, 3, 1);
+
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 2; j++) {
+      printf("%d \n", test_shape[i*2+j]);
+    }
+  }
+
+  printf("Got here 2\n");
+
+  /*
   int nbytes;
   while(1) {
     memset(buffer, 0, BUFFER_SIZE);
@@ -139,5 +166,6 @@ int main() {
   close(listen_fd1);
   close(conn_fd2);
   close(listen_fd2);
+  */
   return EXIT_SUCCESS;
 }
